49940fe0704f8f3a4cd219d264ffab70c5f09b48
Kamil Kryjomski <kryjomskicontact@gmail.com>
2022-11-21 14:02:00 +0100

    [VIAM-53] reasource mapper tests added

--- viam_marine_app/lib/app/domain/resource/model/viam_app_resource_name.dart
+++ viam_marine_app/lib/app/domain/resource/model/viam_app_resource_name.dart
@@ -1,13 +1,24 @@
-class ViamAppResourceName {
+import 'package:equatable/equatable.dart';
+
+class ViamAppResourceName extends Equatable {
   final String namespace;
   final String type;
   final String subtype;
   final String name;
 
-  ViamAppResourceName(
+  const ViamAppResourceName(
     this.namespace,
     this.type,
     this.subtype,
     this.name,
   );
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+        namespace,
+        type,
+        subtype,
+        name,
+      ];
 }
--- viam_marine_app/lib/sdk/src/domain/resource/model/viam_resource_name.dart
+++ viam_marine_app/lib/sdk/src/domain/resource/model/viam_resource_name.dart
@@ -10,4 +10,9 @@ class ViamResourceName {
     this.subtype,
     this.name,
   );
+
+  @override
+  // ignore: hash_and_equals
+  bool operator ==(covariant ViamResourceName other) =>
+      other.name == name && other.namespace == namespace && other.type == type && other.subtype == subtype;
 }
