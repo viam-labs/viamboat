61d91fd6887602d5b64b765eed2a3cf961706e33
Kamil Kryjomski <kryjomskicontact@gmail.com>
2022-11-21 14:02:32 +0100

    [VIAM-53] reasource mapper tests added

---
+++ viam_marine_app/test/unit_test/data/resource/mapper/viam_app_resource_name_to_viam_resource_name_mapper_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:viam_marine/app/data/resource/mapper/viam_app_resource_name_to_viam_resource_name_mapper.dart';
+import 'package:viam_marine/app/domain/resource/model/viam_app_resource_name.dart';
+import 'package:viam_marine/sdk/viam_sdk.dart';
+
+void main() {
+  late ViamAppResourceNameToViamResourceNameMapper mapper;
+
+  setUp(() {
+    mapper = ViamAppResourceNameToViamResourceNameMapper();
+  });
+
+  test("test", () {
+    const ViamAppResourceName dto = ViamAppResourceName(
+      's',
+      't',
+      'st',
+      'n',
+    );
+
+    final expectedAnswer = ViamResourceName(dto.namespace, dto.type, dto.subtype, dto.name);
+
+    final acutal = mapper(dto);
+
+    expect(acutal, expectedAnswer);
+  });
+}
