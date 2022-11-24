5b4f524df5e21cbdd1cc227f93013e4f4e6edf6a
Kamil Kryjomski <kryjomskicontact@gmail.com>
2022-11-24 16:47:46 +0100

    [VIAM-55] added app sensor mapper tests

--- viam_marine_app/lib/app/domain/sensor/model/viam_app_sensor_readings.dart
+++ viam_marine_app/lib/app/domain/sensor/model/viam_app_sensor_readings.dart
@@ -1,15 +1,26 @@
-class ViamAppSensorReadings {
+import 'package:equatable/equatable.dart';
+
+class ViamAppSensorReadings extends Equatable {
   final String namespace;
   final String type;
   final String subtype;
   final String name;
   final Map<String, double> readings;
 
-  ViamAppSensorReadings(
-      this.namespace,
-      this.type,
-      this.subtype,
-      this.name,
-      this.readings,
-      );
+  const ViamAppSensorReadings(
+    this.namespace,
+    this.type,
+    this.subtype,
+    this.name,
+    this.readings,
+  );
+
+  @override
+  List<Object?> get props => [
+        namespace,
+        type,
+        subtype,
+        name,
+        readings,
+      ];
 }
