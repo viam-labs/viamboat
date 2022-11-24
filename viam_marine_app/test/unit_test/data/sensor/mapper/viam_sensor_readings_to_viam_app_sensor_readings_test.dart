0acbc77fbc3e18474ca0086f61d9f0895adf52fb
Kamil Kryjomski <kryjomskicontact@gmail.com>
2022-11-24 16:47:56 +0100

    [VIAM-55] added app sensor mapper tests

---
+++ viam_marine_app/test/unit_test/data/sensor/mapper/viam_sensor_readings_to_viam_app_sensor_readings_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:viam_marine/app/data/sensor/mapper/viam_sensor_readings_to_viam_app_sensor_readings.dart';
+import 'package:viam_marine/app/domain/sensor/model/viam_app_sensor_readings.dart';
+import 'package:viam_marine/sdk/viam_sdk.dart';
+
+void main() {
+  late ViamSensorReadingsToViamAppSensorReadingsMapper viamSensorReadingsToViamAppSensorReadingsMapper;
+
+  setUp(() {
+    viamSensorReadingsToViamAppSensorReadingsMapper = ViamSensorReadingsToViamAppSensorReadingsMapper();
+  });
+
+  group('When map from ViamSensorReading to ViamAppSensorReading', () {
+    test('mapper return correct values', () {
+      final dto = ViamSensorReadings(
+        'namespace',
+        'type',
+        'subtype',
+        'name',
+        {
+          'key': 0.0,
+        },
+      );
+
+      final expectedAnswer = ViamAppSensorReadings(
+        dto.namespace,
+        dto.type,
+        dto.subtype,
+        dto.name,
+        dto.readings,
+      );
+
+      final actualAnswer = viamSensorReadingsToViamAppSensorReadingsMapper(dto);
+
+      expect(actualAnswer, equals(expectedAnswer));
+    });
+  });
+}
