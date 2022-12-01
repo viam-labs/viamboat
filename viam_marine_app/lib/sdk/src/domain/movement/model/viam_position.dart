82f1e7e85303a1609b32367cbbf15991cf7a2ba9
Kamil Kryjomski <kryjomskicontact@gmail.com>
2022-12-01 12:54:01 +0100

    [VIAM-54] add sdk movemnt mapper unit tests

--- viam_marine_app/lib/sdk/src/domain/movement/model/viam_position.dart
+++ viam_marine_app/lib/sdk/src/domain/movement/model/viam_position.dart
@@ -8,4 +8,15 @@ class ViamPosition {
     this.latitude,
     this.longitude,
   );
+
+  @override
+  bool operator ==(covariant ViamPosition other) =>
+      other.altitude == altitude || other.latitude == latitude || other.longitude == longitude;
+
+  @override
+  int get hashCode => Object.hash(
+        altitude,
+        latitude,
+        longitude,
+      );
 }
---
+++ viam_marine_app/lib/sdk/src/test/unit_test/data/movement/mapper/get_position_response_to_viam_position_mapper_test.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: depend_on_referenced_packages
+import 'package:viam_marine/sdk/src/data/movement/mapper/get_position_response_to_viam_position_mapper.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:viam_marine/sdk/src/data/viam/common/v1/common.pb.dart';
+import 'package:viam_marine/sdk/src/data/viam/movementsensor/v1/movementsensor.pb.dart';
+import 'package:viam_marine/sdk/src/domain/movement/model/viam_position.dart';
+
+void main() {
+  late GetPositionResponseToViamPositionMapper getPositionResponseToViamPositionMapper;
+
+  setUp(() {
+    getPositionResponseToViamPositionMapper = GetPositionResponseToViamPositionMapper();
+  });
+
+  group('When map from GetPositionResponse to ViamPosition', () {
+    test('mapper returns correct values', () {
+      final geoPoint = GeoPoint(
+        latitude: 0.0,
+        longitude: 0.0,
+      );
+
+      final dto = GetPositionResponse(
+        altitudeMm: 0.0,
+        coordinate: geoPoint,
+      );
+
+      final expectedAnswer = ViamPosition(
+        dto.altitudeMm,
+        dto.coordinate.latitude,
+        dto.coordinate.longitude,
+      );
+
+      final actualAnswer = getPositionResponseToViamPositionMapper(dto);
+
+      expect(actualAnswer, equals(expectedAnswer));
+    });
+  });
+}
