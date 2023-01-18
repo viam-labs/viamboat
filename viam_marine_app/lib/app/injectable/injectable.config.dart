6d470a116100683bc7c529c390741b805f58a851
Kamil Kryjomski <kryjomskicontact@gmail.com>
2023-01-18 14:37:56 +0100

    [VIAM-69-app] rebase

--- viam_marine_app/lib/app/injectable/injectable.config.dart
+++ viam_marine_app/lib/app/injectable/injectable.config.dart
@@ -11,9 +11,10 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:permission_handler/permission_handler.dart' as _i6;
 import 'package:shared_preferences/shared_preferences.dart' as _i8;
+import 'package:uuid/uuid.dart' as _i9;
 
-import '../../sdk/viam_sdk.dart' as _i31;
-import '../data/boat/service/boat_service_impl.dart' as _i21;
+import '../../sdk/viam_sdk.dart' as _i32;
+import '../data/boat/service/boat_service_impl.dart' as _i22;
 import '../data/boat/store/boat_box_impl.dart' as _i4;
 import '../data/boat/store/shared_prefs_current_boat_store.dart' as _i16;
 import '../data/camera/data_source/camera_api_data_source.dart' as _i33;
@@ -64,6 +65,68 @@ import 'camera_permission_injectable.dart' as _i55;
 import 'navigator_key_injectable.dart' as _i54;
 import 'shared_preferences_injectable.dart' as _i56;
 import 'viam_sdk_injectable/viam_sdk_injectable.dart' as _i57; // ignore_for_file: unnecessary_lambdas
+import '../data/boat/store/shared_prefs_current_boat_store.dart' as _i17;
+import '../data/camera/data_source/camera_api_data_source.dart' as _i34;
+import '../data/camera/mapper/viam_camera_data_to_viam_app_camera_data_mapper.dart'
+    as _i11;
+import '../data/camera/service/camera_service_impl.dart' as _i38;
+import '../data/movement/data_source/movement_sdk_data_source.dart' as _i39;
+import '../data/movement/mapper/viam_position_to_viam_app_position_mapper.dart'
+    as _i12;
+import '../data/movement/service/movement_service_impl.dart' as _i41;
+import '../data/permissions/data_source/camera_permission_data_source.dart'
+    as _i15;
+import '../data/permissions/service/permissions_service_impl.dart' as _i19;
+import '../data/resource/data_source/resource_api_data_source.dart' as _i35;
+import '../data/resource/mapper/viam_app_resource_name_to_viam_resource_name_mapper.dart'
+    as _i10;
+import '../data/resource/mapper/viam_resource_name_to_viam_app_resource_name_mapper.dart'
+    as _i13;
+import '../data/resource/service/resource_service_impl.dart' as _i43;
+import '../data/sensor/data_source/sensor_api_data_source.dart' as _i36;
+import '../data/sensor/mapper/viam_sensor_readings_to_viam_app_sensor_readings.dart'
+    as _i14;
+import '../data/sensor/service/sensor_service_impl.dart' as _i45;
+import '../domain/boat/service/boat_service.dart' as _i21;
+import '../domain/boat/store/boat_box.dart' as _i3;
+import '../domain/boat/store/current_boat_store.dart' as _i16;
+import '../domain/boat/usecase/add_new_boat_use_case.dart' as _i33;
+import '../domain/boat/usecase/check_connection_use_case.dart' as _i23;
+import '../domain/boat/usecase/delete_boat_use_case.dart' as _i24;
+import '../domain/boat/usecase/get_boats_use_case.dart' as _i25;
+import '../domain/boat/usecase/get_current_boat_id_use_case.dart' as _i27;
+import '../domain/boat/usecase/remove_current_boat_id_use_case.dart' as _i28;
+import '../domain/boat/usecase/set_current_boat_id_use_case.dart' as _i29;
+import '../domain/camera/service/camera_service.dart' as _i37;
+import '../domain/camera/usecase/get_camera_data_use_case.dart' as _i47;
+import '../domain/movement/service/movement_service.dart' as _i40;
+import '../domain/movement/usecase/get_position_use_case.dart' as _i48;
+import '../domain/permissions/service/permissions_service.dart' as _i18;
+import '../domain/permissions/usecase/get_camera_permission_status_use_case.dart'
+    as _i26;
+import '../domain/permissions/usecase/request_camera_permission_use_case.dart'
+    as _i20;
+import '../domain/resource/service/resource_service.dart' as _i42;
+import '../domain/resource/usecase/get_resource_names_use_case.dart' as _i49;
+import '../domain/sensor/service/sensor_service_impl.dart' as _i44;
+import '../domain/sensor/usecase/get_sensor_data_use_case.dart' as _i50;
+import '../presentation/page/add_boat/cubit/add_boat_cubit.dart' as _i46;
+import '../presentation/page/dashboard/cubit/dashboard_cubit.dart' as _i54;
+import '../presentation/page/dashboard/widgets/drawer/cubit/viam_drawer_cubit.dart'
+    as _i31;
+import '../presentation/page/scan_qr/cubit/scan_qr_cubit.dart' as _i7;
+import '../presentation/page/splash/cubit/splash_cubit.dart' as _i30;
+import '../presentation/widgets/camera_tile/cubit/camera_tile_cubit.dart'
+    as _i53;
+import '../presentation/widgets/map_tile/cubit/map_tile_cubit.dart' as _i51;
+import '../presentation/widgets/sensor_tile/cubit/sensor_tile_cubit.dart'
+    as _i52;
+import 'camera_permission_injectable.dart' as _i56;
+import 'navigator_key_injectable.dart' as _i55;
+import 'shared_preferences_injectable.dart' as _i57;
+import 'uuid_injectable.dart' as _i58;
+import 'viam_sdk_injectable/viam_sdk_injectable.dart'
+    as _i59; // ignore_for_file: unnecessary_lambdas
 
 const String _dev = 'dev';
 const String _prod = 'prod';
@@ -86,6 +149,7 @@ Future<_i1.GetIt> $initGetIt(
   final navigatorKeyModule = _$NavigatorKeyModule();
   final cameraPermissionModule = _$CameraPermissionModule();
   final sharedPreferencesModule = _$SharedPreferencesModule();
+  final uuidModule = _$UuidModule();
   final viamModule = _$ViamModule();
   gh.singleton<_i3.BoatBox>(_i4.CurrentBoatBoxImpl());
   gh.lazySingleton<_i5.FirebaseAnalytics>(
@@ -120,8 +184,28 @@ Future<_i1.GetIt> $initGetIt(
   gh.factory<_i19.RequestCameraPermissionUseCase>(
       () => _i19.RequestCameraPermissionUseCase(get<_i17.PermissionsService>()));
   gh.factory<_i20.BoatService>(() => _i21.BoatServiceImpl(
+  gh.singleton<_i9.Uuid>(uuidModule.uuidV4);
+  gh.factory<_i10.ViamAppResourceNameToViamResourceNameMapper>(
+      () => _i10.ViamAppResourceNameToViamResourceNameMapper());
+  gh.factory<_i11.ViamCameraDataToViamAppCameraDataMapper>(
+      () => _i11.ViamCameraDataToViamAppCameraDataMapper());
+  gh.factory<_i12.ViamPositionToViamAppPositionMapper>(
+      () => _i12.ViamPositionToViamAppPositionMapper());
+  gh.factory<_i13.ViamResourceNameToViamAppResourceNameMapper>(
+      () => _i13.ViamResourceNameToViamAppResourceNameMapper());
+  gh.factory<_i14.ViamSensorReadingsToViamAppSensorReadingsMapper>(
+      () => _i14.ViamSensorReadingsToViamAppSensorReadingsMapper());
+  gh.factory<_i15.CameraPermissionDataSource>(
+      () => _i15.CameraPermissionDataSource(get<_i6.Permission>()));
+  gh.factory<_i16.CurrentBoatStore>(() =>
+      _i17.SharedPreferencesCurrentBoatStore(get<_i8.SharedPreferences>()));
+  gh.lazySingleton<_i18.PermissionsService>(() =>
+      _i19.PermissionsServiceImpl(get<_i15.CameraPermissionDataSource>()));
+  gh.factory<_i20.RequestCameraPermissionUseCase>(() =>
+      _i20.RequestCameraPermissionUseCase(get<_i18.PermissionsService>()));
+  gh.factory<_i21.BoatService>(() => _i22.BoatServiceImpl(
         get<_i3.BoatBox>(),
-        get<_i15.CurrentBoatStore>(),
+        get<_i16.CurrentBoatStore>(),
       ));
   gh.factory<_i22.CheckConnectionUseCase>(() => _i22.CheckConnectionUseCase(get<_i20.BoatService>()));
   gh.factory<_i23.DeleteBoatUseCase>(() => _i23.DeleteBoatUseCase(get<_i20.BoatService>()));
@@ -134,18 +218,35 @@ Future<_i1.GetIt> $initGetIt(
   gh.factory<_i29.SplashCubit>(() => _i29.SplashCubit(
         get<_i26.GetCurrentBoatIdUseCase>(),
         get<_i24.GetBoatsUseCase>(),
+  gh.factory<_i23.CheckConnectionUseCase>(
+      () => _i23.CheckConnectionUseCase(get<_i21.BoatService>()));
+  gh.factory<_i24.DeleteBoatUseCase>(
+      () => _i24.DeleteBoatUseCase(get<_i21.BoatService>()));
+  gh.factory<_i25.GetBoatsUseCase>(
+      () => _i25.GetBoatsUseCase(get<_i21.BoatService>()));
+  gh.factory<_i26.GetCameraPermissionStatusUseCase>(() =>
+      _i26.GetCameraPermissionStatusUseCase(get<_i18.PermissionsService>()));
+  gh.factory<_i27.GetCurrentBoatIdUseCase>(
+      () => _i27.GetCurrentBoatIdUseCase(get<_i21.BoatService>()));
+  gh.factory<_i28.RemoveCurrentBoatIdUseCase>(
+      () => _i28.RemoveCurrentBoatIdUseCase(get<_i21.BoatService>()));
+  gh.factory<_i29.SetCurrentBoatIdUseCase>(
+      () => _i29.SetCurrentBoatIdUseCase(get<_i21.BoatService>()));
+  gh.factory<_i30.SplashCubit>(() => _i30.SplashCubit(
+        get<_i27.GetCurrentBoatIdUseCase>(),
+        get<_i25.GetBoatsUseCase>(),
       ));
-  gh.factory<_i30.ViamDrawerCubit>(() => _i30.ViamDrawerCubit(
-        get<_i24.GetBoatsUseCase>(),
-        get<_i26.GetCurrentBoatIdUseCase>(),
-        get<_i23.DeleteBoatUseCase>(),
-        get<_i28.SetCurrentBoatIdUseCase>(),
-        get<_i27.RemoveCurrentBoatIdUseCase>(),
+  gh.factory<_i31.ViamDrawerCubit>(() => _i31.ViamDrawerCubit(
+        get<_i25.GetBoatsUseCase>(),
+        get<_i27.GetCurrentBoatIdUseCase>(),
+        get<_i24.DeleteBoatUseCase>(),
+        get<_i29.SetCurrentBoatIdUseCase>(),
+        get<_i28.RemoveCurrentBoatIdUseCase>(),
       ));
-  await gh.singletonAsync<_i31.ViamSdk>(
+  await gh.singletonAsync<_i32.ViamSdk>(
     () => viamModule.getViamSdk(
-      get<_i24.GetBoatsUseCase>(),
-      get<_i26.GetCurrentBoatIdUseCase>(),
+      get<_i25.GetBoatsUseCase>(),
+      get<_i27.GetCurrentBoatIdUseCase>(),
     ),
     preResolve: true,
   );
@@ -162,22 +263,41 @@ Future<_i1.GetIt> $initGetIt(
         get<_i38.ViamAppMovementSdkDataSource>(),
         get<_i9.ViamAppResourceNameToViamResourceNameMapper>(),
         get<_i11.ViamPositionToViamAppPositionMapper>(),
+  gh.factory<_i33.AddNewBoatUseCase>(
+      () => _i33.AddNewBoatUseCase(get<_i21.BoatService>()));
+  gh.factory<_i34.CameraDataSource>(
+      () => _i34.CameraDataSource(get<_i32.ViamSdk>()));
+  gh.factory<_i35.ResourceDataSource>(
+      () => _i35.ResourceDataSource(get<_i32.ViamSdk>()));
+  gh.factory<_i36.SensorDataSource>(
+      () => _i36.SensorDataSource(get<_i32.ViamSdk>()));
+  gh.factory<_i37.ViamAppCameraService>(() => _i38.ViamAppCameraServiceImpl(
+        get<_i34.CameraDataSource>(),
+        get<_i11.ViamCameraDataToViamAppCameraDataMapper>(),
       ));
-  gh.factory<_i41.ViamAppResourceService>(() => _i42.ViamAppResourceServiceImpl(
-        get<_i34.ResourceDataSource>(),
-        get<_i12.ViamResourceNameToViamAppResourceNameMapper>(),
+  gh.factory<_i39.ViamAppMovementSdkDataSource>(
+      () => _i39.ViamAppMovementSdkDataSource(get<_i32.ViamSdk>()));
+  gh.factory<_i40.ViamAppMovementService>(() => _i41.ViamAppMovementServiceImpl(
+        get<_i39.ViamAppMovementSdkDataSource>(),
+        get<_i10.ViamAppResourceNameToViamResourceNameMapper>(),
+        get<_i12.ViamPositionToViamAppPositionMapper>(),
       ));
-  gh.factory<_i43.ViamAppSensorService>(() => _i44.ViamAppSensorServiceImpl(
-        get<_i35.SensorDataSource>(),
-        get<_i9.ViamAppResourceNameToViamResourceNameMapper>(),
-        get<_i13.ViamSensorReadingsToViamAppSensorReadingsMapper>(),
+  gh.factory<_i42.ViamAppResourceService>(() => _i43.ViamAppResourceServiceImpl(
+        get<_i35.ResourceDataSource>(),
+        get<_i13.ViamResourceNameToViamAppResourceNameMapper>(),
       ));
-  gh.factory<_i45.AddBoatCubit>(() => _i45.AddBoatCubit(
-        get<_i32.AddNewBoatUseCase>(),
-        get<_i22.CheckConnectionUseCase>(),
-        get<_i28.SetCurrentBoatIdUseCase>(),
-        get<_i25.GetCameraPermissionStatusUseCase>(),
-        get<_i19.RequestCameraPermissionUseCase>(),
+  gh.factory<_i44.ViamAppSensorService>(() => _i45.ViamAppSensorServiceImpl(
+        get<_i36.SensorDataSource>(),
+        get<_i10.ViamAppResourceNameToViamResourceNameMapper>(),
+        get<_i14.ViamSensorReadingsToViamAppSensorReadingsMapper>(),
+      ));
+  gh.factory<_i46.AddBoatCubit>(() => _i46.AddBoatCubit(
+        get<_i33.AddNewBoatUseCase>(),
+        get<_i23.CheckConnectionUseCase>(),
+        get<_i29.SetCurrentBoatIdUseCase>(),
+        get<_i26.GetCameraPermissionStatusUseCase>(),
+        get<_i20.RequestCameraPermissionUseCase>(),
+        get<_i9.Uuid>(),
       ));
   gh.factory<_i46.GetCameraDataUseCase>(() => _i46.GetCameraDataUseCase(get<_i36.ViamAppCameraService>()));
   gh.factory<_i47.GetPostionUseCase>(() => _i47.GetPostionUseCase(get<_i39.ViamAppMovementService>()));
@@ -193,14 +313,36 @@ Future<_i1.GetIt> $initGetIt(
         get<_i48.GetResourceNamesUseCase>(),
         get<_i24.GetBoatsUseCase>(),
         get<_i26.GetCurrentBoatIdUseCase>(),
+  gh.factory<_i47.GetCameraDataUseCase>(
+      () => _i47.GetCameraDataUseCase(get<_i37.ViamAppCameraService>()));
+  gh.factory<_i48.GetPostionUseCase>(
+      () => _i48.GetPostionUseCase(get<_i40.ViamAppMovementService>()));
+  gh.factory<_i49.GetResourceNamesUseCase>(
+      () => _i49.GetResourceNamesUseCase(get<_i42.ViamAppResourceService>()));
+  gh.factory<_i50.GetSensorDataUseCase>(
+      () => _i50.GetSensorDataUseCase(get<_i44.ViamAppSensorService>()));
+  gh.factory<_i51.MapTileCubit>(() => _i51.MapTileCubit(
+        get<_i48.GetPostionUseCase>(),
+        get<_i50.GetSensorDataUseCase>(),
+      ));
+  gh.factory<_i52.SensorTileCubit>(
+      () => _i52.SensorTileCubit(get<_i50.GetSensorDataUseCase>()));
+  gh.factory<_i53.CameraTileCubit>(
+      () => _i53.CameraTileCubit(get<_i47.GetCameraDataUseCase>()));
+  gh.factory<_i54.DashboardCubit>(() => _i54.DashboardCubit(
+        get<_i49.GetResourceNamesUseCase>(),
+        get<_i25.GetBoatsUseCase>(),
+        get<_i27.GetCurrentBoatIdUseCase>(),
       ));
   return get;
 }
 
-class _$NavigatorKeyModule extends _i54.NavigatorKeyModule {}
+class _$NavigatorKeyModule extends _i55.NavigatorKeyModule {}
 
-class _$CameraPermissionModule extends _i55.CameraPermissionModule {}
+class _$CameraPermissionModule extends _i56.CameraPermissionModule {}
 
-class _$SharedPreferencesModule extends _i56.SharedPreferencesModule {}
+class _$SharedPreferencesModule extends _i57.SharedPreferencesModule {}
 
-class _$ViamModule extends _i57.ViamModule {}
+class _$UuidModule extends _i58.UuidModule {}
+
+class _$ViamModule extends _i59.ViamModule {}
---
+++ viam_marine_app/test/unit_test/presentation/page/add_boat/cubit/add_boat_cubit_test.dart
@@ -0,0 +1,232 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:uuid/uuid.dart';
+import 'package:viam_marine/app/domain/boat/usecase/add_new_boat_use_case.dart';
+import 'package:viam_marine/app/domain/boat/usecase/check_connection_use_case.dart';
+import 'package:viam_marine/app/domain/boat/usecase/set_current_boat_id_use_case.dart';
+import 'package:viam_marine/app/domain/permissions/usecase/get_camera_permission_status_use_case.dart';
+import 'package:viam_marine/app/domain/permissions/usecase/request_camera_permission_use_case.dart';
+import 'package:viam_marine/app/generated/l10n.dart';
+import 'package:viam_marine/app/presentation/page/add_boat/cubit/add_boat_cubit.dart';
+import 'package:viam_marine/app/presentation/page/add_boat/cubit/add_boat_state.dart';
+
+import 'add_boat_cubit_test.mocks.dart';
+
+@GenerateMocks([
+  AddNewBoatUseCase,
+  CheckConnectionUseCase,
+  SetCurrentBoatIdUseCase,
+  GetCameraPermissionStatusUseCase,
+  RequestCameraPermissionUseCase,
+  Uuid,
+])
+Future<void> main() async {
+  late AddBoatCubit addBoatCubit;
+  late AddNewBoatUseCase addNewBoatUseCase;
+  late CheckConnectionUseCase checkConnectionUseCase;
+  late SetCurrentBoatIdUseCase setCurrentBoatIdUseCase;
+  late GetCameraPermissionStatusUseCase getCameraPermissionStatusUseCase;
+  late RequestCameraPermissionUseCase requestCameraPermissionUseCase;
+  late Uuid uuid;
+
+  await Strings.load(const Locale.fromSubtags(languageCode: 'en'));
+
+  setUp(() {
+    addNewBoatUseCase = MockAddNewBoatUseCase();
+    checkConnectionUseCase = MockCheckConnectionUseCase();
+    setCurrentBoatIdUseCase = MockSetCurrentBoatIdUseCase();
+    getCameraPermissionStatusUseCase = MockGetCameraPermissionStatusUseCase();
+    requestCameraPermissionUseCase = MockRequestCameraPermissionUseCase();
+    uuid = MockUuid();
+    addBoatCubit = AddBoatCubit(
+      addNewBoatUseCase,
+      checkConnectionUseCase,
+      setCurrentBoatIdUseCase,
+      getCameraPermissionStatusUseCase,
+      requestCameraPermissionUseCase,
+      uuid,
+    );
+  });
+
+  group('AddBoatCubit', () {
+    const address = 'address';
+    const id = 'id';
+    const name = 'name';
+    const secret = 'secret';
+    const error = 'error';
+    const permissionGrantedStatus = PermissionStatus.granted;
+    const permissionDeniedStatus = PermissionStatus.denied;
+
+    test(
+      'has initial loaded state',
+      () => expect(
+        addBoatCubit.state,
+        const AddBoatState.loaded(canProceed: false),
+      ),
+    );
+
+    group('verifyInputs()', () {
+      blocTest(
+        'emits loaded state with canProceed set to true when fields are not empty',
+        build: () => addBoatCubit,
+        act: (AddBoatCubit cubit) => cubit.verifyInputs(
+          name,
+          address,
+          secret,
+        ),
+        expect: () => [
+          const AddBoatState.loaded(canProceed: true),
+        ],
+      );
+
+      blocTest(
+        'emits loaded state with canProceed set to false when at least one of fields is empty',
+        build: () => addBoatCubit,
+        act: (AddBoatCubit cubit) => cubit.verifyInputs(
+          '',
+          address,
+          '',
+        ),
+        expect: () => [
+          const AddBoatState.loaded(canProceed: false),
+        ],
+      );
+    });
+
+    group('addNewBoat()', () {
+      blocTest(
+        'adds new boat and reloads app.',
+        build: () => addBoatCubit,
+        act: (AddBoatCubit cubit) => cubit.addNewBoat(
+          name,
+          address,
+          secret,
+        ),
+        setUp: () => when(uuid.v4()).thenReturn(id),
+        verify: (_) {
+          verify(checkConnectionUseCase(address, secret));
+          verify(addNewBoatUseCase(
+            address: address,
+            secret: secret,
+            id: id,
+            name: name,
+          ));
+          verify(setCurrentBoatIdUseCase(id));
+        },
+        expect: () => [
+          const AddBoatState.loading(canProceed: false),
+          const AddBoatState.reloadApp(),
+        ],
+      );
+
+      blocTest(
+        'emits error state when connection check fails',
+        build: () => addBoatCubit,
+        act: (AddBoatCubit cubit) => cubit.addNewBoat(
+          name,
+          address,
+          secret,
+        ),
+        setUp: () => when(checkConnectionUseCase(address, secret)).thenAnswer(
+          (_) => Future.error(error),
+        ),
+        verify: (_) => verify(checkConnectionUseCase(address, secret)),
+        expect: () => [
+          const AddBoatState.loading(canProceed: false),
+          const AddBoatState.error(),
+          const AddBoatState.loaded(canProceed: false),
+        ],
+      );
+    });
+
+    group('showConfirmationPopup()', () {
+      blocTest(
+        'shows ConfirmationPopup',
+        build: () => addBoatCubit,
+        act: (AddBoatCubit cubit) => cubit.showConfirmationPopup(),
+        expect: () => [
+          const AddBoatState.showConfirmationPopup(),
+          const AddBoatState.loaded(canProceed: false),
+        ],
+      );
+    });
+
+    group('leavePage()', () {
+      blocTest(
+        'emits leavePage state',
+        build: () => addBoatCubit,
+        act: (AddBoatCubit cubit) => cubit.leavePage(),
+        expect: () => [
+          const AddBoatState.leavePage(),
+        ],
+      );
+    });
+
+    group('showErrorMessage()', () {
+      blocTest(
+        'shows error message',
+        build: () => addBoatCubit,
+        act: (AddBoatCubit cubit) => cubit.showErrorMessage('message'),
+        expect: () => [
+          const AddBoatState.error('message'),
+          const AddBoatState.loaded(canProceed: false),
+        ],
+      );
+    });
+
+    group('scanQrCode()', () {
+      blocTest(
+        'checks camera permissions and navigates to scanQrPage when permissions granted',
+        build: () => addBoatCubit,
+        act: (AddBoatCubit cubit) => cubit.scanQrCode(),
+        setUp: () => when(getCameraPermissionStatusUseCase()).thenAnswer(
+          (_) async => permissionGrantedStatus,
+        ),
+        expect: () => [
+          const AddBoatState.navigateToScanQrPage(),
+        ],
+      );
+
+      blocTest(
+        'requests camera permission and shows error message when not granted',
+        build: () => addBoatCubit,
+        act: (AddBoatCubit cubit) => cubit.scanQrCode(),
+        setUp: () {
+          when(getCameraPermissionStatusUseCase()).thenAnswer(
+            (_) async => permissionDeniedStatus,
+          );
+
+          when(requestCameraPermissionUseCase()).thenAnswer(
+            (_) async => permissionDeniedStatus,
+          );
+        },
+        expect: () => [
+          AddBoatState.error(Strings.current.scan_qr_camera_permissions_denied_msg),
+          const AddBoatState.loaded(canProceed: false),
+        ],
+      );
+
+      blocTest(
+        'requests camera permission and navigates to scanQrPage when granted',
+        build: () => addBoatCubit,
+        act: (AddBoatCubit cubit) => cubit.scanQrCode(),
+        setUp: () {
+          when(getCameraPermissionStatusUseCase()).thenAnswer(
+            (_) async => permissionDeniedStatus,
+          );
+
+          when(requestCameraPermissionUseCase()).thenAnswer(
+            (_) async => permissionGrantedStatus,
+          );
+        },
+        expect: () => [
+          const AddBoatState.navigateToScanQrPage(),
+        ],
+      );
+    });
+  });
+}
