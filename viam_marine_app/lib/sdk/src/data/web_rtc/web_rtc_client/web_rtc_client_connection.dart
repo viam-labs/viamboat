8b1af4d7c1f699e70881418777a7425756ea7350
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2023-01-21 17:22:50 +0100

    unique stream id WIP

--- viam_marine_app/lib/sdk/src/data/web_rtc/web_rtc_client/web_rtc_client_connection.dart
+++ viam_marine_app/lib/sdk/src/data/web_rtc/web_rtc_client/web_rtc_client_connection.dart
@@ -1,14 +1,14 @@
 import 'dart:async';
 
-import 'package:flutter_webrtc/flutter_webrtc.dart';
-import 'package:grpc/grpc_connection_interface.dart';
-import 'package:grpc/grpc.dart';
-
 //ignore: depend_on_referenced_packages
 import 'package:fixnum/fixnum.dart';
-
-import 'package:viam_marine/sdk/src/data/viam/rpc/webrtc/v1/grpc.pb.dart' as grpc;
-import 'package:viam_marine/sdk/src/data/viam/google/protobuf/duration.pb.dart' as grpc_duration;
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:grpc/grpc.dart';
+import 'package:grpc/grpc_connection_interface.dart';
+import 'package:viam_marine/sdk/src/data/viam/google/protobuf/duration.pb.dart'
+    as grpc_duration;
+import 'package:viam_marine/sdk/src/data/viam/rpc/webrtc/v1/grpc.pb.dart'
+    as grpc;
 import 'package:viam_marine/sdk/src/data/web_rtc/web_rtc_client/web_rtc_transport_stream.dart';
 
 class WebRtcClientConnection extends ClientConnection {
@@ -31,6 +31,8 @@ class WebRtcClientConnection extends ClientConnection {
     call.onConnectionReady(this);
   }
 
+  int id = 0;
+
   @override
   GrpcTransportStream makeRequest(
     String path,
@@ -39,15 +41,18 @@ class WebRtcClientConnection extends ClientConnection {
     ErrorHandler onRequestFailure, {
     required CallOptions callOptions,
   }) {
-    final stream = grpc.Stream(id: Int64(0));
-    final grpMetadata = grpc.Metadata(md: metadata.map((key, value) => MapEntry(key, grpc.Strings(values: [value]))));
+    final stream = grpc.Stream(id: Int64(id++));
+    final grpMetadata = grpc.Metadata(
+        md: metadata
+            .map((key, value) => MapEntry(key, grpc.Strings(values: [value]))));
     final grpcTimeout = timeout != null
         ? grpc_duration.Duration(
             seconds: Int64(timeout.inSeconds),
             nanos: timeout.inMicroseconds * 1000,
           )
         : null;
-    final headers = grpc.RequestHeaders(method: path, metadata: grpMetadata, timeout: grpcTimeout);
+    final headers = grpc.RequestHeaders(
+        method: path, metadata: grpMetadata, timeout: grpcTimeout);
     final request = grpc.Request(stream: stream, headers: headers);
     return WebRtcTransportStream(rtcPeerConnection, dataChannel, request);
   }
