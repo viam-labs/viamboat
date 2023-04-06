8c6b05ed7aab2bc4178e834795e3d7cd342a622d
Kamil Kryjomski <kryjomskicontact@gmail.com>
2023-04-06 14:55:40 +0200

    [VIAM-address-fix] Fix robot address

--- viam_marine_app/lib/domain/viam/usecase/get_robot_address_use_case.dart
+++ viam_marine_app/lib/domain/viam/usecase/get_robot_address_use_case.dart
@@ -3,5 +3,5 @@ import 'package:viam_marine/domain/viam/model/robot_address_config.dart';
 
 @injectable
 class GetRobotAddressUseCase {
-  String call(RobotAddressConfig config) => '${config.name}-main.${config.location}.viam.cloud';
+  String call(RobotAddressConfig config) => '${config.name.toLowerCase()}-main.${config.location}.viam.cloud';
 }
