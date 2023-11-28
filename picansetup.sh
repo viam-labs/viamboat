4afc7bd3c3f6431fdebab09f9aaa63cb634acd2d
Eliot Horowitz <eliothorowitz@gmail.com>
2023-11-28 13:25:03 -0500

    oops

--- picansetup.sh
+++ picansetup.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 
-grep spi-bcm2835-overlay /boot/config.txt || printf "dtparam=i2c_arm=on\ndtparam=spi=on\ndtoverlay=mcp2515-can0,oscillator=16000000,interrupt=25\ndtoverlay=spi-bcm2835-overlay\n" >> foo
+grep spi-bcm2835-overlay /boot/config.txt || printf "dtparam=i2c_arm=on\ndtparam=spi=on\ndtoverlay=mcp2515-can0,oscillator=16000000,interrupt=25\ndtoverlay=spi-bcm2835-overlay\n" >> /boot/config.txt
